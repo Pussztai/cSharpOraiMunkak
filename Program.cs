2587f54a802b3255f415eb0b5363fe3c82fa284a
PuszAtiCkik <pusztai.attila@gmail.hu>
2025-09-08 21:35:09 +0200

    asd

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manhattanTavolsag {
+    internal class Program {
+        static void Main(string[] args) {
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Math.Abs(x1 - x2) + Math.Abs(y1 - y2));
+        }
+    }
+}
